13d5c9203770dd30693ae5e1a72a7b548436c124
shihao-song <ss3695@drexel.edu>
2018-12-27 23:26:32 +0800

    Added compilation for pipeline architecture

--- compile.bash
+++ compile.bash
@@ -3,6 +3,11 @@
 if [ "$1" == "single-cycle" ]; then
 	make -C src/single_cycle
 	mv src/single_cycle/RVSim .
+
+elif [ "$1" == "pipeline" ]; then
+	make -C src/pipeline
+	mv src/pipeline/RVSim .
+
 else
 	echo "Unsupported architecture"
 fi
