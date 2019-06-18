82aee6c7da3502af961fa08c2db9c3129149109f
shihao-song <shihao.song@drexel.edu>
2019-06-18 09:44:18 -0400

    Preparing for non C++ feature version

--- compile.bash
+++ compile.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ "$1" == "single-cycle" ]; then
+if [ "$1" == "single-cycle-cpp" ]; then
 	make -C src/single_cycle
 	mv src/single_cycle/RVSim .
+
+elif [ "$1" == "single-cycle-c" ]; then
+        make -C src/single_cycle_c_version
+        mv src/single_cycle_c_version/RVSim .
 
 elif [ "$1" == "pipeline" ]; then
 	make -C src/pipeline
